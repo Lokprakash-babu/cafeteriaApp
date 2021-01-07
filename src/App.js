f846554c0c6d4a85d14502ad2d352b828562b44e
Lokprakash-babu <lokprakash100@gmail.com>
2021-01-07 12:50:25 +0530

    add app bar

--- src/App.js
+++ src/App.js
@@ -4,20 +4,37 @@ import SignUpContainer from './containers/signUpContainer/signUpContainer';
 import { Link, Route, Switch } from 'react-router-dom';
 
 
-class App extends React.Component{
+function App() {
   
-  render(){
+  
     return (
-        <main>
-          <Switch>
-            <Route path="/signUp" component={SignUpContainer} />
-          </Switch>
-        </main>
+        <div>
+        <div data-role="appbar" data-expand-point="md">
+          
+          <ul className="app-bar-menu">
+              <li><Link to="/" onClick={navItemClicked} className="active">Home</Link></li>
+              <li><Link to="/login" onClick={navItemClicked}>Login</Link></li>
+              <li><Link to="/signUp" onClick={navItemClicked}>Sign up</Link></li>
+          </ul>
+        </div>
+          <main>
+            <Switch>
+              <Route path="/signUp" component={SignUpContainer} />
+            </Switch>
+          </main>
+        </div>
       );
-  }
-
 }
 
+function navItemClicked(e){
+  document.getElementsByClassName('active')[0].classList.remove('active');
+  document.getElementsByClassName('app-bar-menu')[0].classList.remove('opened');
+  
+  document.getElementsByClassName('app-bar-menu')[0].classList.add('collapsed');
+  document.getElementsByClassName('app-bar-menu')[0].style.display='none';
+
+  e.target.classList.add('active');
+}
 
 
 export default App;
