a4d901053e9926b949055bd3fb69c8a64a86af46
Lokprakash-babu <lokprakash100@gmail.com>
2021-01-07 11:47:16 +0530

    add react router

--- src/App.js
+++ src/App.js
@@ -1,90 +1,18 @@
 import './App.css';
 import React from 'react';
-import SignUp from './components/Signup/signUp';
+import SignUpContainer from './containers/signUpContainer/signUpContainer';
+import { Link, Route, Switch } from 'react-router-dom';
 
 
 class App extends React.Component{
-  constructor(props) {
-    super(props);
-    this.state = { apiResponse: "" };
-  }
-
-  authFormValidation(){
-      console.log('validation');
-  }
-
-  changed(e){
-    
-    document.getElementById(e.target.id+"Container").innerHTML=e.target.value;
-    
-  }
-
-
-  checkSubmission=(event)=>{
-    event.preventDefault();
-    console.log('verified', event);
-
-    let reader=new FileReader();
-
-    
-    let fullName=document.getElementById('name').value;
-    let email=document.getElementById('mail').value;
-    let orgName=document.getElementById('org').value;
-    let id=document.getElementById('id').value;
-    let mobile=document.getElementById('mobile').value;
-    let img=document.getElementById('photo');
-
-    let body={
-      fullName,
-      email,
-      orgName,
-      id,
-      mobile
-    }
-
-    reader.readAsDataURL(img.files[0]);
-
-    reader.onload=(e)=>{
-      document.getElementById("photoContainer").getElementsByClassName('imgg')[0].setAttribute('src', e.target.result);
-      body['img']=e.target.result;
-      this.callAPI(body);
-    }
-  }
-
-  callAPI(body) {
-
-    console.log('body', body);
-
-    fetch('http://localhost:9000/testAPI', {
-      method: 'POST', 
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(body),
-    })
-    .then(
-      function(data){
-        return data.json();
-      },
-    ).then(
-      function(success){
-        // let data=JSON.parse(success)
-        console.log('success', success);
-      }
-    ).catch(
-      function(err){
-        console.error("error in posting the details",err);
-      }
-    )
-
-  }
-
-
+  
   render(){
     return (
-        <div className="container">
-          <SignUp checkSubmission={this.checkSubmission} changed={this.changed} />
-        </div>
+        <main>
+          <Switch>
+            <Route path="/signUp" component={SignUpContainer} />
+          </Switch>
+        </main>
       );
   }
 
